5c3ded0d732960a17a97071d4102f1ee3758cbd3
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2021-11-21 11:50:49 +0800

    timestamp-microservice
    Back End Development and APIs - Project 1

---
+++ Back End Development and APIs/fcc-timestamp-microservice/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+// enable CORS 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get("/api/:date", function (req, res) {
+  let parsedUnix = parseInt(req.params.date);
+  let utcDate = new Date(req.params.date).toUTCString();
+
+  if(parsedUnix == req.params.date) {
+    res.json({
+      "unix": parsedUnix,
+      "utc": new Date(parsedUnix).toUTCString()
+    });
+  } else if(utcDate != "Invalid Date") {
+    res.json({
+      "unix": Date.parse(utcDate),
+      "utc": utcDate
+    })
+  } else {
+    res.json({
+      "error": "Invalid Date"
+    });
+  }
+
+});
+
+
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

5b837cff86236e60b14238f1696ca8831901e050
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-01-08 00:07:53 +0800

    Information Security with HelmetJS
    Course 1; Part II as bcrypt part

---
+++ Information Security/Information Security with HelmetJS II/server.js
@@ -0,0 +1,61 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
+const app         = express();
+fccTesting(app);
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
+
+//END_ASYNC
+
+//START_SYNC
+
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
+
+//END_SYNC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {});
---
+++ Information Security/Information Security with HelmetJS/myApp.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+
+let ninetyDaysInSeconds = 90*24*60*60;
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});

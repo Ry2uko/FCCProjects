dbe2ebe19161e756627ee41abf61eeec30d98ba2
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-11-14 17:08:36 +0800

    Add Stocks + Chart Improvements

--- Take Home Projects/Chart the Stock Market/routes/api.js
+++ Take Home Projects/Chart the Stock Market/routes/api.js
@@ -5,23 +5,30 @@ import request from 'request';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   let url = 'https://www.alphavantage.co/query';
   let queryFunction = 'TIME_SERIES_MONTHLY',
-  querySymbol = 'TSLA';
+  querySymbol = req.body.symbol || '';
+
+  if (!querySymbol) {
+    return res.status(400).json({ error: 'Stock symbol missing.' });
+  }
 
   // 5 API requests per minute and 500 requests per day
   let apiUrl = `${url}?function=${queryFunction}&symbol=${querySymbol}&apikey=${process.env.STOCK_API_KEY}`;
-  
+
   request.get({
     url: apiUrl,
     json: true,
     headers: { 'User-Agent': 'request'  }
   }, (err, apiRes, data) => {
-    if (err || apiRes.statusCode !==  200) {
+    if (err) {
       res.status(400).json({ error: err });
-    } else {
+    } else if (apiRes.statusCode === 200) {
+      if (data['Error Message']) return res.status(400).json({ error: 'Invalid API call.' });
       res.status(200).json(data);
+    } else {
+      res.status(500).json({ error: `ERROR: HTTP ${apiRes.statusCode}` });
     }
   });
 
